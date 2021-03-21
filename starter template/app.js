1a56adcff2d576040c241c153623f7d7571bf64d
abhishek <abhishekbhati1993@gmail.com>
2021-03-21 10:08:55 +0530

    search updated using city

--- starter template/app.js
+++ starter template/app.js
@@ -1,6 +1,3 @@
-// Tutorial by http://youtube.com/CodeExplained
-// api key : 826ccef81fe193561c53a72451ba51ba
-
 
 //step1. select elements
 const iconElement =document.querySelector('.weather-icon');
@@ -8,6 +5,20 @@ const tempElement=document.querySelector('.temperature-value');
 const tempDescriptionElement=document.querySelector('.temperature-description');
 const locationElement=document.querySelector('.location');
 const notification=document.querySelector('.location');
+const searchBox=document.querySelector(".search-box");
+console.log(searchBox)
+
+//adding keypress eventlener to inputbox
+searchBox.addEventListener("keypress",e=>{
+    //13 is keycode for enter key
+    if(e.keyCode==13){
+        //calling getResults function and passing city name when click enter key
+        getResults(searchBox.value);
+        console.log(searchBox.value)
+    }
+})
+
+
 
 
 //App data
@@ -50,29 +61,49 @@ function getWeather(latitude,longitude){
     //here we use fetch ,fetch return promise
     fetch(api).then((response)=>{
         let data=response.json();
-        console.log(data)
+        // console.log(data)
         return data;
         //here we update our weather object
     }).then((data)=>{
-        weather.temerature.value=Math.floor(data.main.temp-Kelvin);
-        weather.description=data.weather[0].description;
-        weather.icon=data.weather[0].icon;
-        weather.city = data.name;
-        weather.country = data.sys.country;
+      // console.log(data)
+      displayWeather(data)
+      
     //now we need to display weather object data to the user(UI).
-    }).then(()=>{
-            displayWeather();
     })
     .catch(err=>{
         console.log(err)
     })
 }
-function displayWeather(){
+function displayWeather(data){
     //here we change innerHtml of our element
+    weather.temerature.value=Math.floor(data.main.temp-Kelvin);
+    weather.description=data.weather[0].description;
+    weather.icon=data.weather[0].icon;
+    weather.city = data.name;
+    weather.country = data.sys.country;
     iconElement.innerHTML=`<img src="icons/${weather.icon}.png"/>`
     tempElement.innerHTML=`${weather.temerature.value}°<span>C</span>`;
     tempDescriptionElement.innerHTML=`${weather.description}`;
     locationElement.innerHTML = `${weather.city}, ${weather.country}`;
+}
+
+
+function getResults(city){
+let citySearchApi=`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${apiKey}`;
+fetch(citySearchApi)
+// console.log(citySearchApi)
+.then(weather=>{
+    
+    return weather.json();
+
+})
+
+.then((data)=>{
+
+
+  displayWeather(data)
+})//passing response (weather )object
+
 }
 function celeciusToFahrenite(temperature) {
     return (temperature *9/5)+32 ;
@@ -99,3 +130,117 @@ function celeciusToFahrenite(temperature) {
     }
 })
     
+
+// const iconElement = document.querySelector(".weather-icon");
+// const tempElement = document.querySelector(".temperature-value");
+// const tempDescriptionElement = document.querySelector(
+//   ".temperature-description"
+// );
+// const locationElement = document.querySelector(".location");
+// const notification = document.querySelector(".location");
+// const searchBox = document.querySelector(".search-box");
+// const weather = {};
+// weather.temerature = {
+//   unit: "celcius",
+// };
+
+// console.log(searchBox);
+
+// //adding keypress eventlener to inputbox
+// searchBox.addEventListener("keypress", (e) => {
+//   //13 is keycode for enter key
+//   if (e.keyCode == 13) {
+//     //calling getResults function and passing city name when click enter key
+//     getResults(searchBox.value);
+//     console.log(searchBox.value);
+//   }
+// });
+
+// //const kelvin and api key
+// const Kelvin = 273;
+// const apiKey = "826ccef81fe193561c53a72451ba51ba";
+
+// //check browser support geolaction or not
+// if (navigator.geolocation) {
+//   navigator.geolocation.getCurrentPosition(setPosition, showError);
+// } else {
+//   notification.style.display = "block";
+//   notification.innerHTML = "<p>Browser does not sport geolocation</p>";
+// }
+
+// //now we need to set user's position
+// function setPosition(position) {
+//   let latitude = position.coords.latitude;
+//   let longitude = position.coords.longitude;
+
+//   //after getting latitude and longitude we need to call getWeather() function imediately
+//   getWeather(latitude, longitude);
+// }
+// function showError(error) {
+//   notification.innerHTML = `<P>${error.message}</p>`;
+// }
+
+// const setWeatherData = (data) => {
+//   if (!data) {
+//     return;
+//   }
+//   weather.temerature.value = Math.floor(data.main.temp - Kelvin);
+//   weather.description = data.weather[0].description;
+//   weather.icon = data.weather[0].icon;
+//   weather.city = data.name;
+//   weather.country = data.sys.country;
+//   iconElement.innerHTML = `<img src="icons/${weather.icon}.png"/>`;
+//   tempElement.innerHTML = `${weather.temerature.value}°<span>C</span>`;
+//   tempDescriptionElement.innerHTML = `${weather.description}`;
+//   locationElement.innerHTML = `${weather.city}, ${weather.country}`;
+// };
+
+// //getWeather from api provider
+// function getWeather(latitude, longitude) {
+//   let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
+
+//   //here we use fetch ,fetch return promise
+//   fetch(api)
+//     .then((response) => {
+//       let data = response.json();
+//       console.log(data);
+//       return data;
+//       //here we update our weather object
+//     })
+//     .then((data) => {
+//       setWeatherData(data);
+//       //now we need to display weather object data to the user(UI).
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//     });
+// }
+
+// function getResults(city) {
+//   let citySearchApi = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${apiKey}`;
+
+//   fetch(citySearchApi)
+//     .then((response) => response.json())
+//     .then((data) => setWeatherData(data));
+// }
+
+// function celeciusToFahrenite(temperature) {
+//   return (temperature * 9) / 5 + 32;
+// }
+// //when user click on temperature element
+
+// tempElement.addEventListener("click", () => {
+//   if (weather.temerature.value === undefined) {
+//     return;
+//   }
+//   if (weather.temerature.unit == "celcius") {
+//     let fahrenite = celeciusToFahrenite(weather.temerature.value);
+//     fahrenite = Math.floor(fahrenite);
+
+//     tempElement.innerHTML = `${fahrenite}°<span>F</span>`;
+//     weather.temerature.unit = "fahrenite";
+//   } else {
+//     tempElement.innerHTML = `${weather.temerature.value}°<span>C</span>`;
+//     weather.temerature.unit = "celcius";
+//   }
+// });
